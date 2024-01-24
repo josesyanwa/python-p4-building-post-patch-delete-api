ab89d48e17a0c4a389c5549cd39ea22a9d747ddb
Joses Syanwa <josesyanwa@gmail.com>
2024-01-24 20:00:04 +0300

    Completed

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -81,6 +81,68 @@ def users():
     )
 
     return response
+
+@app.route('/reviews/<int:id>', methods=['GET', 'DELETE'])
+def review_by_id(id):
+    review = Review.query.filter_by(id=id).first()
+
+    if request.method == 'GET':
+        review_dict = review.to_dict()
+
+        response = make_response(
+            jsonify(review_dict),
+            200
+        )
+
+        return response
+
+    elif request.method == 'DELETE':
+        db.session.delete(review)
+        db.session.commit()
+
+        response_body = {
+            "delete_successful": True,
+            "message": "Review deleted."    
+        }
+
+        response = make_response(
+            jsonify(response_body),
+            200
+        )
+
+        return response
+
+    elif request.method == 'POST':
+        response_body = {}
+        response = make_response(
+            response_body,
+            201
+        )
+
+        return response
+    
+
+    elif request.method == 'PATCH':
+        review = Review.query.filter_by(id=id).first()
+
+        for attr in request.form:
+            setattr(review, attr, request.form.get(attr))
+
+        db.session.add(review)
+        db.session.commit()
+
+        review_dict = review.to_dict()
+
+        response = make_response(
+            jsonify(review_dict),
+            200
+        )
+
+        return response
+
+
+    
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
